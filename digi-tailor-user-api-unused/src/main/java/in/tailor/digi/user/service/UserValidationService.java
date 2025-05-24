9c80e7d13fadf7085fb0c0cf3f0b209c7efd7a31
ShaikhMazharuddin <mazhar.s2k18@gmail.com>
2025-05-24 14:31:01 +0530

    deleted unused module

--- digi-tailor-user-api-unused/src/main/java/in/tailor/digi/user/service/UserValidationService.java
+++
@@ -1,9 +0,0 @@
-package in.tailor.digi.user.service;
-
-import in.tailor.digi.model.User;
-
-public sealed interface UserValidationService permits UserValidationServiceImpl {
-    String validateSignupUser(User user);
-    String validateUpdateUser(User user);
-    String validatePassword(String password);
-}
