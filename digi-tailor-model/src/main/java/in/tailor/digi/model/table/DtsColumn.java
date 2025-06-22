12895d0738b2ecea1683ff329ea0f87a2840fea7
ShaikhMazharuddin <mazhar.s2k18@gmail.com>
2025-06-22 14:03:20 +0530

    measurement schema refactored

--- digi-tailor-model/src/main/java/in/tailor/digi/model/table/DtsColumn.java
+++ digi-tailor-model/src/main/java/in/tailor/digi/model/table/DtsColumn.java
@@ -1,56 +1,73 @@
 /**
- * 
+ *
  */
 package in.tailor.digi.model.table;
 
 /**
- * 
+ *
  */
 public abstract class DtsColumn {
-//	tables
-	public static final String DTS_SHOP = "DTS_SHOP";
-	public static final String DTS_ADDRESS = "DTS_ADDRESS";
-	public static final String DTS_USER = "DTS_USER";
-	public static final String DTS_USER_SHOP_MAPPING = "DTS_USER_SHOP_MAPPING";
-	public static final String DTS_CUSTOMER = "DTS_CUSTOMER";
-//	columns
-	public static final String ADDRESS_ID = "ADDRESS_ID";
-	public static final String SHOP_ID = "SHOP_ID";
-	public static final String SHOP_NAME = "SHOP_NAME";
-	public static final String SHOP_CODE = "SHOP_CODE";
-	public static final String ADDRESS_LINE_ONE = "ADD_LINE_ONE";
-	public static final String ADDRESS_LINE_TWO = "ADD_LINE_TWO";
-	public static final String CITY = "CITY";
-	public static final String STATE = "STATE";
-	public static final String ZIPCODE = "ZIPCODE";
-	public static final String CREATED_AT = "CREATED_AT";
-	public static final String UPDATED_AT = "UPDATED_AT";
-	public static final String UPDATED_BY = "UPDATED_BY";
-	public static final String USER_ID = "USER_ID";
-	public static final String FIRST_NAME = "FIRST_NAME";
-	public static final String MIDDLE_NAME = "MIDDLE_NAME";
-	public static final String LAST_NAME = "LAST_NAME";
-	public static final String MOBILE_NO = "MOBILE_NO";
-	public static final String EMAIL = "EMAIL";
-	public static final String USER_NAME = "USER_NAME";
-	public static final String PASSWORD = "PASSWORD";
-	public static final String ALTERNATE_MOBILE_NO = "ALTERNATE_MOBILE_NO";
-	public static final String USER_TYPE = "USER_TYPE";
-	public static final String CUSTOMER_ID = "CUSTOMER_ID";
-	public static final String TYPE_ID = "TYPE_ID";
-	public static final String TYPE_NAME = "TYPE_NAME";
-	public static final String TYPE_DESCRIPTION = "TYPE_DESCRIPTION";
-	public static final String COMMENT = "COMMENT";
-	public static final String CREATED_BY = "CREATED_BY";
-	public static final String PRICE = "PRICE";
-	public static final String UDT_ID = "UDT_ID";
-	public static final String DRESS_TYPE_ID = "DRESS_TYPE_ID";
-	public static final String DRESS_ID = "DRESS_ID";
-	public static final String HOLIDAY = "HOLIDAY";
-	public static final String OPEN_TIME = "OPEN_TIME";
-	public static final String CLOSE_TIME = "CLOSE_TIME";
-	public static final String SHOP_STATUS = "SHOP_STATUS";
-	public static final String SHOP_IMAGE = "SHOP_IMAGE";
-	private DtsColumn() {
-	}
+    //	tables
+    public static final String DTS_SHOP = "DTS_SHOP";
+    public static final String DTS_ADDRESS = "DTS_ADDRESS";
+    public static final String DTS_USER = "DTS_USER";
+    public static final String DTS_USER_SHOP_MAPPING = "DTS_USER_SHOP_MAPPING";
+    public static final String DTS_CUSTOMER = "DTS_CUSTOMER";
+    public static final String DTS_MEASUREMENT_ATTRIBUTE = "DTS_MEASUREMENT_ATTRIBUTE";
+    public static final String DTS_MEASUREMENT_ATTRIBUTE_TYPE = "DTS_MEASUREMENT_ATTRIBUTE_TYPE";
+    public static final String DTS_MEASUREMENT = "DTS_MEASUREMENT";
+    //	columns
+    public static final String ADDRESS_ID = "ADDRESS_ID";
+    public static final String SHOP_ID = "SHOP_ID";
+    public static final String SHOP_NAME = "SHOP_NAME";
+    public static final String SHOP_CODE = "SHOP_CODE";
+    public static final String ADDRESS_LINE_ONE = "ADD_LINE_ONE";
+    public static final String ADDRESS_LINE_TWO = "ADD_LINE_TWO";
+    public static final String CITY = "CITY";
+    public static final String STATE = "STATE";
+    public static final String ZIPCODE = "ZIPCODE";
+    public static final String CREATED_AT = "CREATED_AT";
+    public static final String UPDATED_AT = "UPDATED_AT";
+    public static final String UPDATED_BY = "UPDATED_BY";
+    public static final String USER_ID = "USER_ID";
+    public static final String FIRST_NAME = "FIRST_NAME";
+    public static final String MIDDLE_NAME = "MIDDLE_NAME";
+    public static final String LAST_NAME = "LAST_NAME";
+    public static final String MOBILE_NO = "MOBILE_NO";
+    public static final String EMAIL = "EMAIL";
+    public static final String USER_NAME = "USER_NAME";
+    public static final String PASSWORD = "PASSWORD";
+    public static final String ALTERNATE_MOBILE_NO = "ALTERNATE_MOBILE_NO";
+    public static final String USER_TYPE = "USER_TYPE";
+    public static final String CUSTOMER_ID = "CUSTOMER_ID";
+    public static final String TYPE_ID = "TYPE_ID";
+    public static final String TYPE_NAME = "TYPE_NAME";
+    public static final String TYPE_DESCRIPTION = "TYPE_DESCRIPTION";
+    public static final String COMMENT = "COMMENT";
+    public static final String CREATED_BY = "CREATED_BY";
+    public static final String PRICE = "PRICE";
+    public static final String UDT_ID = "UDT_ID";
+    public static final String DRESS_TYPE_ID = "DRESS_TYPE_ID";
+    public static final String DRESS_ID = "DRESS_ID";
+    public static final String HOLIDAY = "HOLIDAY";
+    public static final String OPEN_TIME = "OPEN_TIME";
+    public static final String CLOSE_TIME = "CLOSE_TIME";
+    public static final String SHOP_STATUS = "SHOP_STATUS";
+    public static final String SHOP_IMAGE = "SHOP_IMAGE";
+    public static final String ATTRIBUTE_ID = "ATTRIBUTE_ID";
+    public static final String ATTRIBUTE_NAME = "ATTRIBUTE_NAME";
+    public static final String ATTRIBUTE_DESCRIPTION = "ATTRIBUTE_DESCRIPTION";
+    public static final String ATTRIBUTE_IMAGE = "ATTRIBUTE_IMAGE";
+    public static final String ATTRIBUTE_TYPE_ID = "ATTRIBUTE_TYPE_ID";
+    public static final String ATTRIBUTE_TYPE_NAME = "ATTRIBUTE_TYPE_NAME";
+    public static final String ATTRIBUTE_TYPE_DESCRIPTION = "ATTRIBUTE_TYPE_DESCRIPTION";
+    public static final String ATTRIBUTE_TYPE_IMAGE = "ATTRIBUTE_TYPE_IMAGE";
+    public static final String MEASUREMENT_ID = "MEASUREMENT_ID";
+    public static final String MEASUREMENT_ATTRIBUTE_ID = "MEASUREMENT_ATTRIBUTE_ID";
+    public static final String MEASUREMENT_ATTRIBUTE_TYPE_ID = "MEASUREMENT_ATTRIBUTE_TYPE_ID";
+    public static final String MEASUREMENT_VALUE = "MEASUREMENT_VALUE";
+    public static final String MEASUREMENT_UNIT = "MEASUREMENT_UNIT";
+
+    private DtsColumn() {
+    }
 }
